c776c9c69c45fc63b33700a2ff69938de5d63e75
Nuno Diegues <nmld@ist.utl.pt>
2011-07-14 17:41:04 +0100

    Added the concept of Citation related to an Article Record. Upon submission of a Document of type Article, its content is parsed for citations which are then stored. The display of an article has been improved to show the citations.

--- app/models/User.java
+++ app/models/User.java
@@ -48,6 +48,7 @@ public class User extends Model {
 	}
 
 	public static User connect(String email, String password) {
+		System.out.println(Crypto.passwordHash(password));
 		return find("byEmailAndPassword", email, Crypto.passwordHash(password)).first();
 	}
 
---
+++ app/models/bibliography/Citation.java
@@ -0,0 +1,49 @@
+package models.bibliography;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+@Entity
+public class Citation extends Model implements Comparable<Citation> {
+
+	// 1
+	@Required
+	public int bibNum;
+
+	// "E. Author, B. Author and C. Author. In Proceedings"
+	@Lob
+	@Required
+	public String reference;
+
+	// "In [1] the authors claim that this is good."
+	@Lob
+	@Required
+	public String citation;
+
+	@Required
+	@ManyToOne
+	public ArticleRecord recordThatReferences;
+
+	public Citation(int bibNum, String reference, String citation, ArticleRecord recordThatReferences) {
+		this.bibNum = bibNum;
+		this.reference = reference;
+		this.citation = citation;
+		this.recordThatReferences = recordThatReferences;
+	}
+
+	@Override
+	public int compareTo(Citation o) {
+		if (this.bibNum < o.bibNum) {
+			return -1;
+		} else if (this.bibNum == o.bibNum) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+
+}
